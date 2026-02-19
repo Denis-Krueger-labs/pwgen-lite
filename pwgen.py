596dd38510271c14a5c54ddb9739d2d1de89c433
Denis-Krüger <dkrueger725@gmail.com>
2026-02-19 11:11:00 +0100

    Implemented optional symbol support to password generator

--- pwgen.py
+++ pwgen.py
@@ -6,21 +6,28 @@ Minimalistic password generator in Python.
 import secrets
 import string
 
-# Allowed charchters for now 
-ALPHABET = string.ascii_letters + string.digits
+# Base character sets
+BASE_ALPHABET = string.ascii_letters + string.digits
+SYMBOLS = "!@#$%^&*()-_=+[]{}|;:,.<>?/~`"
 
-def generate_password(length: int) -> str:
+def generate_password(length: int, use_symbols: bool) -> str:
     """
     Generate a password of given length using secure randomness
     """
-    return "".join(secrets.choice(ALPHABET) for _ in range(length))
+    
+    alphabet = BASE_ALPHABET
+
+    if use_symbols:
+        alphabet += SYMBOLS
+
+    return "".join(secrets.choice(alphabet) for _ in range(length))
 
 
 def main():
     raw = input("Enter password length: ").strip()
 
     if not raw.isdigit():
-        print("INvalid input. Please enter a valid positive number. ")
+        print("Invalid input. Please enter a valid positive number. ")
         return
     
     length = int(raw)
@@ -29,7 +36,11 @@ def main():
         print("Length must be greater then 0.")
         return 
     
-    password = generate_password(length)
+    symbol_choice = input("Include symbols? (y/n):").strip().lower()
+    use_symbols = symbol_choice == 'y'
+
+
+    password = generate_password(length, use_symbols)
     print("Generated password:", password)
 
     
