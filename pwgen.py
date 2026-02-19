d9cd530158a75bc570b8b9f51205c7c62a7abbb3
Denis-Krüger <dkrueger725@gmail.com>
2026-02-19 13:20:49 +0100

    Add support for excluding ambiguous characters in password generation and update tests

--- pwgen.py
+++ pwgen.py
@@ -12,12 +12,15 @@ import string
 BASE_ALPHABET = string.ascii_letters + string.digits
 SYMBOLS = "!@#$%^&*()-_=+[]{}|;:,.<>?/~`"
 
-def generate_password(length: int, use_symbols: bool) -> str:
+def generate_password(length: int, use_symbols: bool, exclude_ambiguous: bool) -> str:
     """
     Generate a password of given length using secure randomness
     """
     
     alphabet = BASE_ALPHABET + (SYMBOLS if use_symbols else "")
+    for ch in"O0Il1":
+        alphabet = alphabet.replace(ch, "") 
+
     return "".join(secrets.choice(alphabet) for _ in range(length))
 
 def parse_arguments():
@@ -29,6 +32,7 @@ def parse_arguments():
 
     parser.add_argument("length", type=int, help="Length of the password (positive integer)")
     parser.add_argument("--symbols", action="store_true", help="Include symbols in the password")
+    parser.add_argument("--exclude-ambigous", action="store_true", help="Exclude ambiguous characters (O,0,l,I,1)")
     return parser.parse_args()                    
 
 def main():
@@ -38,7 +42,7 @@ def main():
         print("Error: Length must be a positive integer.")
         return
     
-    print(generate_password(args.length, args.symbols))
+    print(generate_password(args.length, args.symbols, args.exclude_ambigous))
     
     
 if __name__ == "__main__":
